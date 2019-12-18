498d137cb20a090dc0abca810a0f68c0647ef1fe
Pastew <kpastew@gmail.com>
2019-12-18 22:53:42 +0100

    done

--- KolkoIKrzyzyk2/Form1.Designer.cs
+++ KolkoIKrzyzyk2/Form1.Designer.cs
@@ -28,13 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRuch = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(22, 12);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(58, 58);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Czyj ruch:";
+            // 
+            // txtRuch
+            // 
+            this.txtRuch.AutoSize = true;
+            this.txtRuch.Location = new System.Drawing.Point(108, 218);
+            this.txtRuch.Name = "txtRuch";
+            this.txtRuch.Size = new System.Drawing.Size(16, 13);
+            this.txtRuch.TabIndex = 10;
+            this.txtRuch.Text = "...";
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(86, 12);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(58, 58);
+            this.btn2.TabIndex = 11;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(150, 12);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(58, 58);
+            this.btn3.TabIndex = 12;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(22, 76);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(58, 58);
+            this.btn4.TabIndex = 13;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(86, 76);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(58, 58);
+            this.btn5.TabIndex = 14;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(150, 76);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(58, 58);
+            this.btn6.TabIndex = 15;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(22, 140);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(58, 58);
+            this.btn7.TabIndex = 16;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(86, 140);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(58, 58);
+            this.btn8.TabIndex = 17;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(150, 140);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(58, 58);
+            this.btn9.TabIndex = 18;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(226, 243);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.txtRuch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtRuch;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
     }
 }
 
--- KolkoIKrzyzyk2/Form1.cs
+++ KolkoIKrzyzyk2/Form1.cs
@@ -12,9 +12,188 @@ namespace KolkoIKrzyzyk2
 {
     public partial class Form1 : Form
     {
+        //zmienne ktore tu są, będą widoczne we wszystkich metodach w tej klasie
+        private bool czyj_ruch = true; //true to X, false to Y
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void WstawZnak(object przycisk)
+        {
+            Button wcisnietyPrzycisk = (Button)przycisk;
+            // sender to obiekt,który został wciśnięty, musimy go zrzutować
+            // na typ Button(przycisk), aby mozna było ustawić mu znak i ew. wyłączyć
+
+            if (czyj_ruch == true)
+            {
+                wcisnietyPrzycisk.Text = "X";
+            }
+            else
+            {
+                wcisnietyPrzycisk.Text = "O";
+            }
+
+            wcisnietyPrzycisk.Enabled = false;
+
+            bool wynik = SprawdzCzyKtosWygral();
+            if (wynik == true)
+            {
+                string tekstWygranej;
+                if (czyj_ruch == true)
+                {
+                    tekstWygranej = "Wygrał Gracz X! Rozpocząć jeszcze raz?";
+                }
+                else
+                {
+                    tekstWygranej = "Wygrał Gracz O! Rozpocząć jeszcze raz?";
+                }
+
+                //Sprawdzamy, czy gracze chcą kontynuować czy nie
+                DialogResult odpowiedz = MessageBox.Show(tekstWygranej, "Wygrana", MessageBoxButtons.YesNo);
+
+                if (odpowiedz == DialogResult.No)
+                {
+                    WylaczWszystkiePrzyciski();
+                }
+                else
+                {
+                    WlaczWszystkiePrzyciskiIResetuj();
+                }
+            }
+
+            czyj_ruch = !czyj_ruch; //zmieniamy z true na false lub z false na true,
+
+            //zmieniemay aktualnego gracza
+            if (czyj_ruch == true)
+            {
+                txtRuch.Text = "X";
+            }
+            else
+            {
+                txtRuch.Text = "O";
+            }
+
+        }
+        private void WlaczWszystkiePrzyciskiIResetuj()
+        {
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+            btn9.Enabled = true;
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+        }
+        private void WylaczWszystkiePrzyciski()
+        {
+            btn1.Enabled = false;
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+            btn4.Enabled = false;
+            btn5.Enabled = false;
+            btn6.Enabled = false;
+            btn7.Enabled = false;
+            btn8.Enabled = false;
+            btn9.Enabled = false;
+        }
+        private bool SprawdzCzyKtosWygral()
+        {
+            //Winning Condition For First Row   
+            if (btn1.Text == btn2.Text && btn2.Text == btn3.Text && btn1.Text != "")
+            //wystarczy sprawdzić tylko jeden, czy nie jest pusty
+            {
+                return true;
+            }
+            else if (btn4.Text == btn5.Text && btn5.Text == btn6.Text && btn4.Text != "")
+            {
+                return true;
+            }
+            else if (btn7.Text == btn8.Text && btn8.Text == btn9.Text && btn7.Text != "")
+            {
+                return true;
+            }
+            else if (btn1.Text == btn4.Text && btn4.Text == btn7.Text && btn1.Text != "")
+            {
+                return true;
+            }
+            else if (btn2.Text == btn5.Text && btn5.Text == btn8.Text && btn2.Text != "")
+            {
+                return true;
+            }
+            else if (btn3.Text == btn6.Text && btn6.Text == btn9.Text && btn3.Text != "")
+            {
+                return true;
+            }
+            else if (btn1.Text == btn5.Text && btn5.Text == btn9.Text && btn1.Text != "")
+            {
+                return true;
+            }
+            else if (btn3.Text == btn5.Text && btn5.Text == btn7.Text && btn3.Text != "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
         }
     }
 }
